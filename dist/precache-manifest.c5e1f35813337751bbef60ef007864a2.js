ae1511efd6a2f7575dc9e01a443281091153e7e9
kamleshgehlot <kamlesh.gehlott@gmail.com>
2020-06-23 22:44:16 +0530

    update

---
+++ dist/precache-manifest.c5e1f35813337751bbef60ef007864a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f34a23363b2997629a87e358ebbce42c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a5599568f1e6b11a4198",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4282ed13af7bbb2278c4",
+    "url": "/static/js/2.a15082fa.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a15082fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5599568f1e6b11a4198",
+    "url": "/static/js/main.54013d82.chunk.js"
+  },
+  {
+    "revision": "e30ddc1f5db99dbf7fc3",
+    "url": "/static/js/runtime-main.2a0bead7.js"
+  }
+]);
