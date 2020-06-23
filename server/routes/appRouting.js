2b58d4cb2abfd4ad94194a86a1c329ce66df4045
kamleshgehlot <kamlesh.gehlott@gmail.com>
2020-06-23 22:42:45 +0530

    first

--- server/routes/appRouting.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const AppController = require('../controllers/AppController.js');
-
-const Routing = express.Router();
-
-Routing.route("/sendMail").post(AppController.sendMail);
-
-module.exports = Routing;
--- src/api/config/HttpClient.js
+++
@@ -1,8 +0,0 @@
-import * as c from './Constants';
-
-export default function checkError(error) {
-  if(error || error.response.status === 401) {
-    c.APP_TOKEN.remove();
-    document.location.reload();
-  }
-}
